73c17f45c6874c54ffce1e223b5a13481874543b
Juha Paananen <juha.paananen@gmail.com>
2013-09-23 23:07:33 +0300

    working working

--- OegySync.hs
+++ OegySync.hs
@@ -4,11 +4,38 @@ import Data.Aeson.Generic(decode)
 import Data.Data(Data, Typeable)
 import Data.Maybe(fromJust)
 import qualified Data.ByteString.Lazy as L8
+import Path
+import System.Process(readProcessWithExitCode)
+import System.Exit
+import Data.List(inits)
 
-data Path = Path { local :: String, remote :: String } deriving (Show, Data, Typeable)
-data Conf = Conf { localRoot :: String, remoteRoot :: String, paths :: [Path] } deriving (Show, Data, Typeable)
+data PathPair = Path { local :: Path, remote :: Path } deriving (Show, Data, Typeable)
+data Conf = Conf { root :: PathPair, paths :: [PathPair] } deriving (Show, Data, Typeable)
 
 main = do
   string <- L8.readFile ".oegysyncrc"
   let conf = fromJust $ decode string :: Conf
-  putStrLn $ localRoot conf
+  mapM_ (sync conf) (paths conf)
+
+sync :: Conf -> PathPair -> IO ()
+sync conf path = do
+  let localPath = subPath (local $ root $ conf) (local path)
+  let remotePath = subPath (remote $ root $ conf) (remote path)
+  putStrLn $ "Syncing " ++ localPath ++ " <-> " ++ remotePath
+  rsync path (root conf)
+
+rsync :: PathPair -> PathPair -> IO ()
+rsync path root = do
+    let includes = []
+    let excludes = ["--exclude='*'", "--exclude='.DS_Store'"]
+    let paths = [remote root, local root]
+    let rsyncOptions = ["-ruh", "--progress", "--include='*.*'"] ++ includes ++ excludes ++ paths
+    putStrLn $ show rsyncOptions
+    result <- readProcessWithExitCode "echo" rsyncOptions []
+    case result of
+        (ExitSuccess, output, _) -> do
+          putStrLn "done"
+          putStrLn output
+
+rsyncIncludes :: [String] -> [String]
+rsyncIncludes pathElems = map joinPaths $ filter (/= []) $ inits pathElems
---
+++ Path.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE FlexibleInstances, TypeSynonymInstances, OverlappingInstances #-}
+
+module Path where
+
+import Data.List
+import ListUtil
+
+type Path = String
+
+class Pathy a where
+    pathOf :: a -> Path
+    
+instance Pathy Path where
+    pathOf path = path
+    
+samePath :: Pathy a => Pathy b => a -> b -> Bool
+samePath a b = pathOf a == pathOf b
+
+parentOf :: Pathy a => Pathy b => a -> b -> Bool
+parentOf parent child = (pathOf parent) ++ "/" `elem` inits (pathOf child)
+
+parent :: Pathy a => a -> Path
+parent path = reverse $ (drop n) $ reverse $ pathOf path
+    where n = 1 + length (lastPathElement path)
+
+subPath :: Pathy a => Pathy b => a -> b -> Path
+subPath root sub = (pathOf root) ++ "/" ++ (pathOf sub)
+
+lastPathElement :: Pathy a => a -> Path
+lastPathElement path = last $ split '/' (pathOf path)
+
+firstPathElement :: Pathy a => a -> Path
+firstPathElement path = head $ split '/' (pathOf path)
+
+isAbsolutePath :: Pathy a => a -> Bool
+isAbsolutePath path = (pathOf path) `startsWith` "/"
+
+joinPaths :: [Path] -> Path
+joinPaths [] = ""
+joinPaths [a] = a
+joinPaths (a:rest) = a ++ "/" ++ (joinPaths rest)
